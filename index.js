888c7fba09da8b794714cf1e4bce45c8a2d37794
Colin Busch <colinbusch@icloud.com>
2018-07-11 00:22:35 +0200

    svg viewer responsive

---
+++ index.js
@@ -0,0 +1,9 @@
+// setInterval(function () {
+//   if(document.getElementById('love1').className === 'disabled'){
+//     document.getElementById('love2').className = 'disabled';
+//     document.getElementById('love1').className = '';
+//   }else {
+//     document.getElementById('love1').className = 'disabled';
+//     document.getElementById('love2').className = '';
+//   }
+// }, 2000);
